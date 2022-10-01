20c6cc91313b9deee8ff8e12693e675104a84366
nguyenthikimsen <kimsen3077@gmail.com>
2022-10-01 14:01:08 +0700

    sortByPrice

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import Product from '../../components/Product/Product';
+import _ from 'lodash';
 
 let timeout = null;
 
 export default function Search() {
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState({});
   const [arrProductSearch, setArrProductSearch] = useState([]);
+  const [valueSort, setValueSort] = useState('desc');
 
   const handleChange = (e) => {
     const { value } = e.target;
@@ -24,7 +26,6 @@ export default function Search() {
         url: `https://shop.cyberlearn.vn/api/Product?keyword=${search.keyword}`,
         method: 'GET',
       });
-      console.log(result.data.content);
       // console.log(search.keyword);
       setArrProductSearch(result.data.content);
     } catch (err) {
@@ -33,7 +34,10 @@ export default function Search() {
   };
 
   const renderProductSearch = () => {
-    return arrProductSearch.map((prod, index) => {
+    const resultAscendingPrice = _.sortBy(arrProductSearch, [(item) => item.price]);
+    const resultDecreasePrice = _.reverse(_.sortBy(arrProductSearch, [(item) => item.price]));
+
+    return (valueSort === 'desc' ? resultDecreasePrice : resultAscendingPrice).map((prod, index) => {
       return <Product prod={prod} key={index} />;
     });
   };
@@ -58,9 +62,15 @@ export default function Search() {
           <button className="search-btn btnSubmit">SEARCH</button>
           <h2 className="search-title productFeature-title mb-0">Search result</h2>
           <p className="search-p mt-4">Price</p>
-          <select className="search-select search-input form-select" aria-label="Default select example">
-            <option value="decrease">Decrease</option>
-            <option value="ascending">Ascending</option>
+          <select
+            className="search-select search-input form-select"
+            aria-label="Default select example"
+            onChange={(e) => setValueSort(e.target.value)}
+          >
+            {/* giam */}
+            <option value="desc">Decrease</option>
+            {/* tang */}
+            <option value="asc">Ascending</option>
           </select>
           <div className="row gy-4 productFeature-group">{renderProductSearch()}</div>
         </div>
